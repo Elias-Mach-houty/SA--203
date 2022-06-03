054cf2777e0800eb16090105e8b712af99f7fcf2
Elias-Mach-houty <elias.mach-houty@edu.univ-fcomte.fr>
2022-06-03 15:15:41 +0200

    Changement de nom

--- src/components/BilleterieAccView.vue
+++ src/components/BilleterieAccView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-bleu text-center mx-auto mt-14 w-3/4">
+    <div class="bg-bleu text-center mx-auto mt-14 w-3/4  xl:w-1/2 ">
         <img class="mx-auto w-full object-cover" src="/src/assets/billeterie festoche 1.jpg" alt="">
         <p  class="w-64 mx-auto mt-2 text-center font-Corps text-black">La billetterie est ouverte ! N’hésitez pas à prendre 
             vos places pour les Vezeday’s avant qu’il ne soit trop tard !</p>
